a52055430800c444d5569c48bed7b25ab870d08b
Kelton <keltonfernandes@live.com>
2025-01-26 20:44:32 -0100

    feat: add navigation bar component for improved site navigation

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <nav className="shadow-lg">
+            <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+                <Link href="/">
+                    <h1 className="text-xl font-semibold ">Task Manager</h1>
+                </Link>
+                <div className="space-x-6">
+                    <Link href="/task" legacyBehavior>
+                        <a className="text-gray-700 hover:text-gray-300">Task Board</a>
+                    </Link>
+                    <Link href="/profile" legacyBehavior>
+                        <a className="text-gray-700 hover:text-gray-300">User Profile</a>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
