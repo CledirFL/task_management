480acd9c66e0c60bdb24d606685a6abc126f33fd
Kelton <keltonfernandes@live.com>
2025-01-27 07:43:02 -0100

    feat: add DrawerMenu to NavBar for mobile and tablet navigation

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
+import DrawerMenu from '../DrawerMenu'
 
 export default function NavBar() {
     return (
-        <nav className="shadow-lg">
+        <nav className="shadow-lg ">
             <div className="container mx-auto px-4 py-4 flex justify-between items-center">
                 <Link href="/">
                     <h1 className="text-xl font-semibold ">Task Manager</h1>
                 </Link>
-                <div className="space-x-6">
+                <div className="space-x-6 hidden md:block">
                     <Link href="/task" legacyBehavior>
                         <a className="text-gray-700 hover:text-gray-300">Task Board</a>
                     </Link>
@@ -16,6 +17,8 @@ export default function NavBar() {
                         <a className="text-gray-700 hover:text-gray-300">User Profile</a>
                     </Link>
                 </div>
+                 {/* Drawer Menu for Mobile/Tablet */}
+               <DrawerMenu />
             </div>
         </nav>
     )
