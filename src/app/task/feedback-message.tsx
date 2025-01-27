afbf6e527b87da64f7655df460050659c9943208
Kelton <keltonfernandes@live.com>
2025-01-27 09:37:34 -0100

    refactor: simplify FeedbackMessage component by removing size prop and adjusting layout

--- src/app/task/feedback-message.tsx
+++ src/app/task/feedback-message.tsx
@@ -5,12 +5,11 @@ interface FeedbackMessageProps {
     feedbackMessage: string;
     deletedTask?: Task | null;
     undoDelete?: () => void;
-    size?: 'small' | 'large';
 }
 
-export default function FeedbackMessage({ feedbackMessage, deletedTask, undoDelete, size = 'small' }: FeedbackMessageProps) {
+export default function FeedbackMessage({ feedbackMessage, deletedTask, undoDelete, }: FeedbackMessageProps) {
     return (
-        <div className={`px-4 py-3 rounded mb-4 ${size === 'small' ? 'w-full' : 'w-1/5'} mt-5 ${deletedTask ? 'bg-red-100 border border-red-400 text-red-700' : 'bg-green-100 border border-green-400 text-green-700'}`}>
+        <div className={`px-4 py-3 rounded mb-4 w-full md:w-1/3 mt-5 ${deletedTask ? 'bg-red-100 border border-red-400 text-red-700' : 'bg-green-100 border border-green-400 text-green-700'}`}>
             {feedbackMessage}
             {deletedTask && (
                 <button
