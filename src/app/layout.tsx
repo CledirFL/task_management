697abb1a78aa23964ea138ff6dcdfd5dfe2518ed
Kelton <keltonfernandes@live.com>
2025-01-27 07:41:34 -0100

    feat: integrate Footer component into layout for consistent site structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Navigation Bar */}
-        <NavBar />
-        {children}
+        <div className="min-h-screen flex flex-col font-[family-name:var(--font-geist-sans)]">
+          {/* Navigation Bar */}
+          <NavBar />
+          <main className="flex-grow ">
+            {children}
+          </main>
+
+          {/* Footer */}
+          <Footer />
+        </div>
+
       </body>
     </html>
   );
