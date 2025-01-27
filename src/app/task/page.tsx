e3dc8ae057833f66d3e7c0cd4b66e72e4a7fc1a0
Kelton <keltonfernandes@live.com>
2025-01-27 08:45:39 -0100

    feat: implement Task interface and refactor TaskPage for improved task management

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -1,13 +1,11 @@
 "use client"
-import Modal from '@/components/Modal';
 import { useState } from 'react';
+import Modal from '@/components/Modal';
+import FeedbackMessage from './feedback-message';
+import { Task } from '../lib/interface';
+import TaskNotFound from './task-not-found';
+import TaskList from './task-list';
 
-interface Task {
-    id?: number;
-    title: string;
-    description: string;
-    status: 'To Do' | 'In Progress' | 'Done';
-}
 
 const sampleTasks: Task[] = [
     { id: 1, title: 'Task 1', description: 'Description for task 1', status: 'To Do' },
@@ -81,6 +79,12 @@ const TaskPage = () => {
         setIsModalOpen(false);
         setNewTask({ id: undefined, title: '', description: '', status: 'To Do' });
     }
+    const handleCreateNew = () => {
+        setFeedbackMessage(null);
+        setNewTask({ id: undefined, title: '', description: '', status: 'To Do' });
+        setIsModalOpen(true);
+
+    }
     const filteredTasks = filter === 'All' ? tasks : tasks.filter(task => task.status === filter);
 
     return (
@@ -88,17 +92,7 @@ const TaskPage = () => {
             <h1 className='text-2xl font-bold mb-4'>Task Management</h1>
             <p className='text-gray-400'>Manage your tasks here.</p>
             {feedbackMessage && (
-                <div className={`px-4 py-3 rounded mb-4 w-1/3 ${deletedTask ? 'bg-red-100 border border-red-400 text-red-700' : 'bg-green-100 border border-green-400 text-green-700'}`}>
-                    {feedbackMessage}
-                    {deletedTask && (
-                        <button
-                            className="ml-4 text-blue-500 underline"
-                            onClick={undoDelete}
-                        >
-                            Undo
-                        </button>
-                    )}
-                </div>
+                <FeedbackMessage size='large' feedbackMessage={feedbackMessage} undoDelete={undoDelete} deletedTask={deletedTask} />
             )}
             {filteredTasks.length != 0 && (<div className="flex gap-3 justify-end mb-4">
                 <div>
@@ -179,62 +173,14 @@ const TaskPage = () => {
                     </div>
                 </form>
                 {feedbackMessage && !newTask.id && (
-                    <div className="mt-5 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">
-                        {feedbackMessage}
-                    </div>
+                    <FeedbackMessage feedbackMessage={feedbackMessage}  />
                 )}
             </Modal>
 
             {filteredTasks.length === 0 ? (
-                <div className="text-center mt-8">
-                    <p className="text-gray-400 mb-4">No tasks available. Create a new task to get started.</p>
-                    <button
-                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={() => setIsModalOpen(true)}
-                    >
-                        Create New Task
-                    </button>
-                </div>
+               <TaskNotFound onCreateNew={handleCreateNew} />
             ) : (
-                <div className='flex gap-4 w-full p-4 rounded shadow bg-gray-800 overflow-x-auto'>
-                    <table className='table-auto w-full'>
-                        <thead>
-                            <tr className="">
-                                <th className="px-4 py-2">Title</th>
-                                <th className="px-4 py-2">Description</th>
-                                <th className="px-4 py-2">Status</th>
-                                <th className="px-4 py-2">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody className=''>
-                            {filteredTasks.map(task => (
-                                <tr key={task.id} className="border-b">
-                                    <td className="px-4 py-2 text-center">{task.title}</td>
-                                    <td className="px-4 py-2 text-center">{task.description}</td>
-                                    <td className="px-4 py-2 text-center whitespace-nowrap">
-                                        <span className={`px-2 py-1 rounded-full text-white ${task.status === 'Done' ? 'bg-green-500' : task.status === 'In Progress' ? 'bg-blue-500' : 'bg-gray-500'}`}>
-                                            {task.status}
-                                        </span>
-                                    </td>
-                                    <td className="px-4 py-2 text-center flex gap-3 justify-center">
-                                        <button
-                                            className="bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
-                                            onClick={() => handleEditTask(task)}
-                                        >
-                                            Edit
-                                        </button>
-                                        <button
-                                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                                            onClick={() => deleteTask(task.id!)}
-                                        >
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                <TaskList filteredTasks={filteredTasks} handleEditTask={handleEditTask} deleteTask={deleteTask} />
             )}
         </div>
     );
