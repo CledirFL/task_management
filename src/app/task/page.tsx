dc9fbc8f709c8172ede4171cb869aefc55a7722d
Kelton <keltonfernandes@live.com>
2025-01-27 09:37:39 -0100

    refactor: remove sampleTasks array and adjust FeedbackMessage component props

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -8,15 +8,6 @@ import TaskList from './task-list';
 import TaskForm from './task-form';
 import TaskFilter from './task-filter';
 
-
-const sampleTasks: Task[] = [
-    { id: 1, title: 'Task 1', description: 'Description for task 1', status: 'To Do' },
-    { id: 2, title: 'Task 2', description: 'Description for task 2', status: 'In Progress' },
-    { id: 3, title: 'Task 3', description: 'Description for task 3', status: 'Done' },
-    { id: 4, title: 'Task 4', description: 'Description for task 4', status: 'To Do' },
-    { id: 5, title: 'Task 5', description: 'Description for task 5', status: 'In Progress' }
-];
-
 const TaskPage = () => {
     const [tasks, setTasks] = useState<Task[]>(() => {
         const savedTasks = localStorage.getItem('tasks');
@@ -101,7 +92,7 @@ const TaskPage = () => {
             <h1 className='text-2xl font-bold mb-4'>Task Management</h1>
             <p className='text-gray-400'>Manage your tasks here.</p>
             {feedbackMessage &&
-                <FeedbackMessage size='large' feedbackMessage={feedbackMessage} undoDelete={undoDelete} deletedTask={deletedTask} />
+                <FeedbackMessage feedbackMessage={feedbackMessage} undoDelete={undoDelete} deletedTask={deletedTask} />
             }
 
             {filteredTasks.length != 0 && <TaskFilter filter={filter} setFilter={setFilter} setIsModalOpen={setIsModalOpen} />}
