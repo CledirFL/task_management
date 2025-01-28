da8f0af104675a2f1080700b9b3b99d7af832603
Kelton <keltonfernandes@live.com>
2025-01-28 08:36:18 -0100

    feat: integrate SideBar component into RootLayout for improved navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import SideBar from "@/components/SideBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,22 +25,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="min-h-screen flex flex-col font-[family-name:var(--font-geist-sans)]">
-          {/* Navigation Bar */}
-          <NavBar />
-          <main className="flex-grow ">
-            {children}
+        <div className="flex min-h-screen">
+          {/* Sidebar */}
+          <SideBar />
+          {/* Main Content */}
+          <main className="flex w-full flex-col p-6 justify-between ">
+            <NavBar />
+            <div className="flex-1">
+              {children}
+            </div>
+            <Footer />
           </main>
-
-          {/* Footer */}
-          <Footer />
         </div>
-
       </body>
     </html>
   );
