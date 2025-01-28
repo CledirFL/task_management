cda13f5d9da1b67b59c4899869d5907e2f393477
Kelton <keltonfernandes@live.com>
2025-01-28 08:36:35 -0100

    refactor: enhance UserProfile component layout and structure for better readability

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -23,56 +23,60 @@ const UserProfile: React.FC = () => {
     }
 
     return (
-        <div className="max-w-md mx-auto mt-10 p-5 border rounded-lg shadow-lg">
-            <div className="flex flex-col items-center">
-                <Image
-                    src="https://picsum.photos/150"
-                    alt="Profile"
-                    width={128}
-                    height={128}
-                    className="w-32 h-32 rounded-full mb-4"
-                />
-                {editMode ? (
-                    <form onSubmit={handleSave} className="w-full">
-                        <div className="mb-4">
-                            <label className="block text-gray-700">Name</label>
-                            <input
-                                type="text"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                required
-                                minLength={3}
-                                className="w-full px-3 py-2 border rounded-lg bg-gray-800 border-gray-800"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-gray-700">Email</label>
-                            <input
-                                type="email"
-                                value={email}
-                                required
-                                onChange={(e) => setEmail(e.target.value)}
-                                className="w-full px-3 py-2 border rounded-lg bg-gray-800 border-gray-800"
-                            />
-                        </div>
-                        <div className="flex gap-4">
-                            <Button type='submit'  rounded='rounded-md'>Save</Button>
-                            <Button onClick={handleCancel} variant='outline' rounded='rounded-md'>cancel</Button>
-                        </div>
+        <div>
+            <h1 className='text-2xl font-bold mb-4'>Profile Page</h1>
+            <div className="max-w-md mx-auto mt-10 p-5 border rounded-lg shadow-lg">
+                <div className="flex flex-col items-center">
+                    <Image
+                        src="https://picsum.photos/150"
+                        alt="Profile"
+                        width={128}
+                        height={128}
+                        className="w-32 h-32 rounded-full mb-4"
+                    />
+                    {editMode ? (
+                        <form onSubmit={handleSave} className="w-full">
+                            <div className="mb-4">
+                                <label className="block text-gray-700">Name</label>
+                                <input
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    required
+                                    minLength={3}
+                                    className="w-full px-3 py-2 border rounded-lg bg-gray-800 border-gray-800"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700">Email</label>
+                                <input
+                                    type="email"
+                                    value={email}
+                                    required
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="w-full px-3 py-2 border rounded-lg bg-gray-800 border-gray-800"
+                                />
+                            </div>
+                            <div className="flex gap-4">
+                                <Button type='submit' rounded='rounded-md'>Save</Button>
+                                <Button onClick={handleCancel} variant='outline' rounded='rounded-md'>cancel</Button>
+                            </div>
 
-                    </form>
-                ) : (
-                    <div className="text-center flex    flex-col">
-                        <h2 className="text-2xl font-bold">{name}</h2>
-                        <p className="text-gray-700">{email}</p>
-                        <div className='mt-4 px-4 self-center'>
-                            <Button onClick={() => setEditMode(true)}>Edit Profile</Button>
-                        </div>
+                        </form>
+                    ) : (
+                        <div className="text-center flex    flex-col">
+                            <h2 className="text-2xl font-bold">{name}</h2>
+                            <p className="text-gray-700">{email}</p>
+                            <div className='mt-4 px-4 self-center'>
+                                <Button onClick={() => setEditMode(true)}>Edit Profile</Button>
+                            </div>
 
-                    </div>
-                )}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
+
     );
 };
 
