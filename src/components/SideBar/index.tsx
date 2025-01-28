ff0907ba17058293ef302e60ecd250183082b3ed
Kelton <keltonfernandes@live.com>
2025-01-28 08:35:40 -0100

    feat: add SideBar component with navigation links for Task Board and User Profile

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,34 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+export default function SideBar() {
+    const pathName = usePathname();
+    const isActive = (path: string) => pathName === path;
+
+    return (
+        <aside className="w-64 bg-background shadow-lg text-white border-r border-gray-900 hidden md:block">
+            <div className="p-4 font-bold text-xl border-b border-gray-800">
+                <Link href="/">
+                    Task Manager
+                </Link>
+
+            </div>
+            <nav className="mt-5">
+                <ul className='flex flex-col gap-2'>
+                    <li className="mb-2 ml-4">
+                        <Link href="/task" legacyBehavior>
+                            <a className={`text-gray-700 hover:text-gray-300 ${isActive('/task') ? 'font-bold text-gray-200' : ''}`}>Task Board</a>
+                        </Link>
+                    </li>
+                    <li className="mb-2 ml-4">
+                        <Link href="/profile" legacyBehavior>
+                            <a className={`text-gray-700 hover:text-gray-300 ${isActive('/profile') ? 'font-bold text-gray-200' : ''}`} >User Profile</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
